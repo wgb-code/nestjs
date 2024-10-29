813f18a4d2e36055277506474d5412c48200c9ee
wgb-code <front.willian@gmail.com>
2024-10-28 21:44:17 -0300

    Validando os campos.

--- src/dtos/create-student-dto.ts
+++ src/dtos/create-student-dto.ts
@@ -1,5 +1,21 @@
+import { IsEmail, IsNotEmpty, IsNumber, Length } from "class-validator"
+
 export class CreateStudentDTO {
+
+    @IsNotEmpty({
+        message: 'Name is mandatory'
+    })
+    @Length(3, 60, {
+        message: "The name must be between 3 and 60 letters"
+    })
     name?: string
+
+    @IsNotEmpty({
+        message: 'Email is mandatory'
+    })
+    @IsEmail()
     email?: string
+
+    @IsNumber()
     age?: number
 }
